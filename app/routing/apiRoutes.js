8a6c965bef4487a630af8f3ad760638095913955
matthewmahler <matthewjamesmahler@gmail.com>
2018-04-29 16:26:10 -0400

    broke it

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,27 @@
 var friendData = require("../data/friends.js");
+var newFriend;
 
-module.exports = function(app) {
-  app.get("/api/friends", function(req, res) {
+module.exports = function (app) {
+  app.get("/api/friends", function (req, res) {
     res.json(friendData);
   });
-  
-  app.post("/api/friends", function(req, res) {    
-      friendData.push(req.body);
-      res.json(true);
+
+  app.post("/api/friends", function (req, res) {
+    friendData.push(req.body);
+    res.json(true);
+    var newFriend = req.body;
+    findDiff(newFriend);
   });
 };
+
+
+function findDiff(newFriend) {
+  var totalDiff = 0;
+  var questionDiff = 0;
+  for (var i = 0; i < friendData.length; i++) {
+    for (var j = 0; i < friendData[i].scores.length; j++) {
+      questionDiff = parseInt(friendData[i].scores[j]) - parseInt(newFriend.scores[j])
+      totalDiff = totalDiff + questionDiff     
+    }   
+  };
+}
