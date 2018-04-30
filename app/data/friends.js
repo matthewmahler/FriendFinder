65101e950b1bfcf86e1871dfacab2dec5d028e90
matthewmahler <matthewjamesmahler@gmail.com>
2018-04-30 08:03:35 -0400

    idk whats going on anymore

--- app/data/friends.js
+++ app/data/friends.js
@@ -31,6 +31,54 @@ var friendsArray = [
       5
     ]
   },
+  {
+    "name": "Kevin",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      3,
+      4,
+      5,
+      2,
+      4,
+      3,
+      5,
+      3,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Will",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      5,
+      4,
+      5,
+      2,
+      5,
+      1,
+      1,
+      1,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Steve",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [
+      4,
+      4,
+      5,
+      2,
+      4,
+      1,
+      4,
+      3,
+      4,
+      5
+    ]
+  },
 ];
 
 module.exports = friendsArray;
